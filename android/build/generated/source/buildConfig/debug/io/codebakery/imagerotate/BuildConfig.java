d06b275fd9d53953bdfcc3b93211e7f9d627fa28
Simon Buerger <simon@bluegrassdigital.com>
2023-10-31 17:18:43 +0200

    push

---
+++ android/build/generated/source/buildConfig/debug/io/codebakery/imagerotate/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package io.codebakery.imagerotate;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "io.codebakery.imagerotate";
+  public static final String BUILD_TYPE = "debug";
+}
